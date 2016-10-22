ac5bb1e5ebfcab9bf92eebbfe1015722caa6f43c
Jarvis <jarvisna@miamioh.edu>
2016-10-22 12:36:39 -0400

    Moved Stuff

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -3,17 +3,17 @@ using System.Collections;
 
 public class Movement : MonoBehaviour {
 
-    public GameObject walls;
+    public int speed;
 
 	// Use this for initialization
 	void Start ()
     {
-
-        	
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        var move = new Vector3(1, 0, 0);
+        transform.position += move * speed * Time.deltaTime;
 	}
 }
