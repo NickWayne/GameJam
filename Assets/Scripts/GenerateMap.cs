eb042e010dc16abd494bf8d05e08bc8f5de3a450
Jarvis <jarvisna@miamioh.edu>
2016-10-22 13:39:35 -0400

    Scrolling Background Works

---
+++ Assets/Scripts/GenerateMap.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GenerateMap : MonoBehaviour {
+
+    public GameObject Locker;
+    public GameObject Background;
+
+    private Random random;
+    private float BackgroundWidth;
+    private float BackgroundHeight;
+
+    // Use this for initialization
+    void Start () {
+        random = new Random();
+
+
+        BackgroundWidth = Background.GetComponent<SpriteRenderer>().sprite.bounds.size.x;
+        BackgroundHeight = Background.GetComponent<SpriteRenderer>().sprite.bounds.size.y;
+
+        int numBackground = 15;
+        for (int i = 0; i < numBackground; i++) {
+            Instantiate(Background, (new Vector3(-6 + i * BackgroundWidth, BackgroundHeight / 2, 0)), Quaternion.identity);
+        }
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,16 +4,31 @@ using System.Collections;
 public class Movement : MonoBehaviour {
 
     public int speed;
+    private bool grounded;
+    public GameObject ground;
 
 	// Use this for initialization
 	void Start ()
     {
-        
+        grounded = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         var move = new Vector3(1, 0, 0);
         transform.position += move * speed * Time.deltaTime;
-	}
+
+
+
+        if (Input.GetKeyDown("space") && grounded)
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 250));
+            grounded = false;
+        }
+
+        else if (GetComponent<Rigidbody2D>().IsTouching(ground.GetComponent<BoxCollider2D>()))
+        {
+            grounded = true;
+        }
+    }
 }
